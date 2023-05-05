f824c1586aa65ad18fc9f8a3512a439c672b83b9
keilalopez03 <keila.magali2003@hotmail.com>
2023-05-05 15:17:46 -0300

    index nuevo

--- emmabeauty/js/main.js
+++ emmabeauty/js/main.js
@@ -1,4 +1,20 @@
-function inicio (){
-    var Nombreusuario  = prompt ("introduce tu nombre:")
-    alert ( "bienvenido" + Nombreusuario + "!")
+function inicio() {
+    let Nombreusuario = prompt("introduce tu nombre:")
+    alert("bienvenido/a" + Nombreusuario + "!")
+}
+console.log(Nombreusuario("keila"))
+let uñas = "1"
+let Pestañas = "2"
+let Cejas = "3"
+let Salir = "4"
+
+const secciones = (1, 2, 3, 4, op)
+switch (op) {
+    case "1"
+    prompt:
+        ("desea solicitar turno?")
+        break;
+
+    default:
+        break;
 }
