6071b80e3537b6eb73aebf24ae70a42d2b2138bc
keilalopez03 <keila.magali2003@hotmail.com>
2023-05-05 20:05:46 -0300

    iconos y bucle

--- emmabeauty/js/main.js
+++ emmabeauty/js/main.js
@@ -12,6 +12,7 @@ nombre()
 
 function servicios() {
     let opcionSeleccionada = prompt(`Hola ${nombreUsuario}, seleccione el servicio \n 1:uñas \n 2:pestañas \n 3:cejas \n 4:salir`)
+    while (opcionSeleccionada !=4 )
     switch (opcionSeleccionada) {
         case uñas:
             tiposUñas();
@@ -28,21 +29,22 @@ function servicios() {
     }
 }
 
+
 function tiposUñas() {
     console.log("Apretaste uñas")
-    let opcionSeleccionada = prompt("Escriba el servicio de uñas a realizarse")
+    let opcionSeleccionada = prompt("Escriba el servicio de uñas a realizarse \n -semipermanente \n -Esculpidas \n -soft gel")
     alert(`${nombreUsuario}, comuniquese para sacar su turno de ${opcionSeleccionada} `)
 }
 
 function tiposPestañas() {
     console.log("Apretaste pestañas")
-    let opcionSeleccionada = prompt("Escriba el servicio de Pestañas a realizarse")
+    let opcionSeleccionada = prompt("Escriba el servicio de Pestañas a realizarse \n -Lifting \n -Service pestañas \n -Extensiones")
     alert(`${nombreUsuario}, comuniquese para sacar su turno de ${opcionSeleccionada} `)
 }
 
 function tiposCejas() {
 
     console.log("Apretaste cejas")
-    let opcionSeleccionada = prompt("Escriba el servicio de cejas a realizarse")
+    let opcionSeleccionada = prompt("Escriba el servicio de cejas a realizarse \n -Perfilado \n -Henna \n -Laminado")
     alert(`${nombreUsuario}, comuniquese para sacar su turno de ${opcionSeleccionada} `)
 }
