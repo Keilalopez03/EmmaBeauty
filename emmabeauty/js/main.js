0c99b4367d712a9666e585adf616bf1175080cad
keilalopez03 <keila.magali2003@hotmail.com>
2023-05-05 17:06:54 -0300

    funciones index

--- emmabeauty/js/main.js
+++ emmabeauty/js/main.js
@@ -1,23 +1,48 @@
-let Nombreusuario;
+let nombreUsuario;
+let uñas = "1"
+let pestañas = "2"
+let cejas = "3"
 
-function inicio() {
-    Nombreusuario = prompt("introduce tu nombre:")
-    alert("Bienvenid@" + " " + Nombreusuario + "!")
-    console.log(Nombreusuario)
+function nombre() {
+    nombreUsuario = prompt("introduce tu nombre:")
+    alert(`Bienvenid@ ${nombreUsuario}!`)
+    console.log(nombreUsuario)
 }
-inicio()
- // let uñas = "1"
- // let Pestañas = "2"
- // let Cejas = "3"
- // let Salir = "4"
+nombre()
 
- // const secciones = (1, 2, 3, 4, op)
- // switch (op) {
- //     case "1"
- //     prompt:
- //         ("desea solicitar turno?")
- //         break;
+function servicios() {
+    let opcionSeleccionada = prompt(`Hola ${nombreUsuario}, seleccione el servicio \n 1:uñas \n 2:pestañas \n 3:cejas \n 4:salir`)
+    switch (opcionSeleccionada) {
+        case uñas:
+            tiposUñas();
+            break;
+        case pestañas:
+            tiposPestañas();
+            break;
+        case cejas:
+            tiposCejas()
+            break;
+        default:
+            console.log("Apretaste salir")
+            break;
+    }
+}
 
- //     default:
- //         break;
- // }
+function tiposUñas() {
+    console.log("Apretaste uñas")
+    let opcionSeleccionada = prompt("Escriba el servicio de uñas a realizarse")
+    alert(`${nombreUsuario}, comuniquese para sacar su turno de ${opcionSeleccionada} `)
+}
+
+function tiposPestañas() {
+    console.log("Apretaste pestañas")
+    let opcionSeleccionada = prompt("Escriba el servicio de Pestañas a realizarse")
+    alert(`${nombreUsuario}, comuniquese para sacar su turno de ${opcionSeleccionada} `)
+}
+
+function tiposCejas() {
+
+    console.log("Apretaste cejas")
+    let opcionSeleccionada = prompt("Escriba el servicio de cejas a realizarse")
+    alert(`${nombreUsuario}, comuniquese para sacar su turno de ${opcionSeleccionada} `)
+}
